77ff8b03a116155c65187ee2ac2437217ed60120
alexrnguyen <alexander.nguyen@shaw.ca>
2024-01-04 17:04:41 -0700

    Add status container to GamePage

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,7 +1,63 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
+import PropTypes from 'prop-types';
 import getImageId from "../utils/get-image-id";
-import { CircularProgress } from "@mui/material";
+import { ButtonGroup, IconButton, CircularProgress, Card, Rating } from "@mui/material";
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import QueueIcon from '@mui/icons-material/Queue';
+import CakeIcon from '@mui/icons-material/Cake';
+
+
+const StatusContainer = (props) => {
+    StatusContainer.propTypes = {
+        playedStatus: PropTypes.bool.isRequired,
+        backlogStatus: PropTypes.bool.isRequired,
+        wishlistStatus: PropTypes.bool.isRequired
+    }
+    const {playedStatus, backlogStatus, wishlistStatus} = props;
+
+    const [playedStatusState, setPlayedStatusState] = useState(playedStatus);
+    const [backlogStatusState, setBacklogStatusState] = useState(backlogStatus);
+    const [wishlistStatusState, setWishlistStatusState] = useState(wishlistStatus);
+    const [rating, setRating] = useState(4);
+    function togglePlayedStatus() {
+        setPlayedStatusState(!playedStatusState);
+    }
+
+    function toggleBacklogStatus() {
+        setBacklogStatusState(!backlogStatusState);
+    }
+
+    function toggleWishlistStatus() {
+        setWishlistStatusState(!wishlistStatusState);
+    }
+
+    return (
+        <Card variant="outlined">
+                <Rating
+                    name="personal-rating"
+                    value={rating}
+                    precision={0.5}
+                    size="large"
+                    onChange={(event, newRating) => setRating(newRating)}
+                />
+                <ButtonGroup>
+                    <IconButton aria-label="played" onClick={() => togglePlayedStatus()}>
+                        <SportsEsportsIcon id="played-icon" color={playedStatus ? "success" : "inherit"} />
+                        <p>Played</p>
+                    </IconButton>
+                    <IconButton aria-label="backlog" onClick={() => toggleBacklogStatus()}>
+                        <QueueIcon id="backlog-icon" color={backlogStatus ? "info" : "inherit"}/>
+                        <p>Backlog</p>
+                    </IconButton>
+                    <IconButton aria-label="wishlist" onClick={() => toggleWishlistStatus()}>
+                        <CakeIcon id="wishlist-icon" color={wishlistStatus ? "warning" : "inherit"}/>
+                        <p>Wishlist</p>
+                    </IconButton>
+                </ButtonGroup>
+        </Card>
+    )
+}
 
 const GamePage = () => {
     const [gameData, setGameData] = useState({
@@ -14,7 +70,25 @@ const GamePage = () => {
     const [dataRetrieved, setDataRetrieved] = useState(false);
     const {id} = useParams();
 
+    const playedStatus = useRef(false);
+    const backlogStatus = useRef(false);
+    const wishlistStatus = useRef(false);
+
     useEffect(() => {
+        async function getGameData() {
+            const gameResponse = await fetch(`http://localhost:5000/game/${id}`);
+            if (gameResponse.status === 200) {
+                let data = await gameResponse.json();
+                console.log(data);
+                const imageId = await getImageId(id);
+                data = {...data, imageId};
+                setDataRetrieved(true);
+                return data;
+            } else {
+                return null;
+            }
+        }
+
         const fetchData = async () => {
             const queriedData = await getGameData();
             console.log(queriedData);
@@ -23,27 +97,17 @@ const GamePage = () => {
         fetchData();
     }, [id]);
 
-
-    async function getGameData() {
-        const gameResponse = await fetch(`http://localhost:5000/game/${id}`);
-        if (gameResponse.status === 200) {
-            let data = await gameResponse.json();
-            console.log(data);
-            const imageId = await getImageId(id);
-            data = {...data, imageId};
-            setDataRetrieved(true);
-            return data;
-        } else {
-            return null;
-        }
-    }
-
     return (
         <>
             {dataRetrieved ? (
                 <>
                     <h1>{gameData[0].name}</h1>
                     <img className="card-game-image" src={`https://images.igdb.com/igdb/image/upload/t_cover_big/${gameData.imageId}.png`} alt="" />
+                    <StatusContainer 
+                        playedStatus={playedStatus.current} 
+                        backlogStatus={backlogStatus.current} 
+                        wishlistStatus={wishlistStatus.current}
+                    />
                     <p>{gameData[0].summary}</p>
                 </>
             ) : <CircularProgress/>}
