ba33f08eeb1ebf4f35967de7909e785773a25bc9
alexrnguyen <alexander.nguyen@shaw.ca>
2024-06-11 23:19:03 -0600

    implement sign up backend

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,24 @@
 
 const express = require("express");
 const cors = require("cors");
-const config = require("./config.js")
+const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const config = require("./config.js");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 console.log(config.clientID);
 const PORT = process.env.PORT || 5000;
 const resultLimit = 50;
 var headers = new Headers();
+
+// MongoDB Models
+const User = require("./models/User.js");
 
 headers.append("Client-ID", config.clientID);
 headers.append("Authorization", config.authorization);
@@ -45,11 +54,35 @@ app.get("/companies/:id", async (req, res) => {
   res.status(200).json({name: data[0].name});
 })
 
+app.post("/signup", (req, res) => {
+  console.log(req.body);
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (username === undefined || email === undefined || password === undefined) {
+    res.status(400).send({message: "Missing required field in request body (username, email, and/or password)"});
+    return;
+  }
+  
+
+  bcrypt.hash(password, 10, function(err, hash) {
+    if (err) {
+      res.status(500).send({message: "Failed to register user."});
+      return;
+    }
+    const user = new User({username, email, password_hash: hash});
+    user.save();
+    res.status(201).redirect('/');
+  })
+})
+
 app.get("/sign-out", (req, res) => {
   localStorage.setItem("username", null);
   res.redirect('/');
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Listening on port ${PORT}`);
+  await mongoose.connect(config.atlasURI).then(() => console.log("Connected to database")).catch((err) => console.log(err));
 });
