615e1eb277da4d86d5a278f8628360c9cf561794
alexrnguyen <alexander.nguyen@shaw.ca>
2024-06-27 22:34:44 -0600

    add pagination to search results

--- backend/routes/games.js
+++ backend/routes/games.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const headers = require("../headers");
-const resultLimit = 50;
+const resultLimit = 48;
 
 // MongoDB Models
 const UserGameStatus = require("../models/UserGameStatus.js");
@@ -9,13 +9,23 @@ const UserGameStatus = require("../models/UserGameStatus.js");
 // Search for games based on search input query parameter
 router.get("/", async (req, res) => {
     const searchInput = req.query.searchInput;
+    const page = req.query.page;
     const url = "https://api.igdb.com/v4/games/";
+
+    console.log(`fields name, cover.*; search "${searchInput}"; limit ${resultLimit}; offset ${page ? (page-1)*resultLimit : 0}`);
     const response = await fetch(url, {
       method: "POST",
       headers: headers,
-      body: `fields name, cover.*; search "${searchInput}"; limit ${resultLimit};`,
+      body: `fields name, cover.*; search "${searchInput}"; limit ${resultLimit}; offset ${(page-1)*resultLimit};`,
     });
-    res.status(200).json(await response.json());
+
+    const games = await response.json();
+    
+    // Get number of games (for pagination)
+    const count = response.headers.get("x-count");
+    const numPages = Math.ceil(count / resultLimit);
+
+    res.status(200).json({games, numPages, count});
 });
 
 // Retrieve game details for a game with a given id
@@ -47,15 +57,18 @@ router.get("/:id/ratings", async (req, res) => {
     numRatings[rating] ? numRatings[rating] += 1 : numRatings[rating] = 1;
   }
 
-  // Fill possible ratings (from 1/2 star to 5 stars) with 0s if necessary
+  // Create dataset containing an array of objects with the number of ratings and the rating itself
+  const ratingsDataset = [];
   const possibleRatings = [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5];
   for (const rating of possibleRatings) {
     if (numRatings[rating] === undefined) {
-      numRatings[rating] = 0;
+      ratingsDataset.push({'rating': rating, 'numRatings': 0});
+    } else {
+      ratingsDataset.push({'rating': rating, 'numRatings': numRatings[rating]});
     }
   }
 
-  res.status(200).send({averageRating: averageRating, ratings: numRatings});
+  res.status(200).send({averageRating: averageRating, ratings: ratingsDataset});
 });
 
 module.exports = router;
--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -1,56 +1,58 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useSearchParams } from "react-router-dom";
 import GameCard from "../components/GameCard";
 import { CircularProgress } from "@mui/material";
 import Pagination from '@mui/material/Pagination';
 
-const ResultsContent = (props) => {
-    const {results, input} = props;
-
-    return (
-        <>
-            <h1 className="pl-8">{results.length > 0 ? `Found ${results.length} results matching "${input}"` : `No games found matching "${input}"`} </h1>
-            <ul className="grid grid-cols-auto-fill-200 place-items-center gap-4 py-2">
-                {results.map(game => {
-                    return (
-                        <GameCard key={game.id} gameId={game.id} imageId={game.cover ? game.cover.image_id : null} title={game.name}/>
-                    )
-                })}
-            </ul>
-            <div className="flex justify-center py-2">
-                <Pagination />
-            </div>
-        </>
-    )
-}
-
 const SearchResults = () => {
-    const [results, setResults] = useState([])
+    const [results, setResults] = useState([]);
+    const [numResults, setNumResults] = useState(0);
     const [searchParams] = useSearchParams();
-    const [input, setInput] = useState(searchParams.get("input"))
+    const input = useRef(searchParams.get("input"))
     const [dataRetrieved, setDataRetrieved] = useState(false);
+    const numPages = useRef(1);
+    const [page, setPage] = useState(1);
 
     useEffect(() => {
-        setInput(searchParams.get("input"))
         const fetchData = async () => {
-            const data = await getData(input)
+            const data = await getData(input.current, page);
             console.log(data);
-            setResults(data)
+            setResults(data);
         }
         fetchData();
-    }, [input])
+    }, [input, page])
 
-    async function getData(input = "") {
-        const response = await fetch(`http://localhost:5000/api/games?searchInput=${input}`)
+    async function getData(input = "", page) {
+        const response = await fetch(`http://localhost:5000/api/games?searchInput=${input}&page=${page}`);
         const data = await response.json();
+        const games = data.games;
+        numPages.current = data.numPages;
+        setNumResults(data.count);
         setDataRetrieved(true);
-        return data;
+        return games;
+    }
+
+    function handlePageChange(event, value) {
+        console.log("Page changed!", value);
+        setPage(value);
     }
 
     return (
         <>
             {dataRetrieved ? (
-                <ResultsContent results={results} input={input}/>
+                <>
+                    <h1 className="pl-8">{results.length > 0 ? `Found ${numResults} results matching "${input.current}"` : `No games found matching "${input}"`} </h1>
+                    <ul className="grid grid-cols-auto-fill-200 place-items-center gap-4 py-2">
+                        {results.map(game => {
+                            return (
+                                <GameCard key={game.id} gameId={game.id} imageId={game.cover ? game.cover.image_id : null} title={game.name}/>
+                            )
+                        })}
+                    </ul>
+                    <div className="flex justify-center py-2">
+                        <Pagination count={numPages.current} page={page} onChange={handlePageChange} />
+                    </div>
+                </>
             ) : <div className="h-screen flex items-center justify-center"><CircularProgress/></div>}
         </>
     )
