63c0acc56784fcfd6886784d4df2184924160755
alexrnguyen <alexander.nguyen@shaw.ca>
2024-07-13 15:53:05 -0600

    show game card title above card on hover

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,16 +1,29 @@
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate} from "react-router-dom";
 
+// Displays game cover and title
 const GameCard = ({gameId, imageId, title}) => {
+
+    const [isHovering, setIsHovering] = useState(false);
+
     const navigate = useNavigate();
     function handleClick() {
         console.log("Clicked!")
         navigate(`/game/${gameId}`)
     }
 
+    function handleMouseOver() {
+        setIsHovering(true);
+    }
+
+    function handleMouseOut() {
+        setIsHovering(false);
+    }
+
     return (
-        <div className="flex flex-col justify-center w-52 h-64" onClick={handleClick}>
-            <img className="max-h-52 object-contain" src={`https://images.igdb.com/igdb/image/upload/t_cover_big/${imageId}.png`} alt="" />
-            <div className="card-game-title text-center">{title}</div>
+        <div className="relative hover:border-1 hover:border-red hover:rounded-lg" onClick={handleClick} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+            <img className="max-h-52 object-contain rounded-md" src={`https://images.igdb.com/igdb/image/upload/t_cover_big/${imageId}.png`} alt={`${title} Cover`} />
+            {isHovering && <div className="game-card-title">{title}</div>}
         </div>
     )
 }
