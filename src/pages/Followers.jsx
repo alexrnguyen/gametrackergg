062885ad6df30014aedaed22b718696af7daf0e1
alexrnguyen <alexander.nguyen@shaw.ca>
2024-07-31 21:57:56 -0600

    follower/following page frontend

--- src/pages/Followers.jsx
+++ src/pages/Followers.jsx
@@ -1,14 +1,28 @@
 import { useParams } from "react-router-dom";
 import UserGrid from "../components/UserGrid";
+import { useEffect, useState } from "react";
 
 const Followers = () => {
-    const {uid} = useParams();
+    const { uid } = useParams();
+
+    const [followers, setFollowers] = useState([]);
     
-    // Temporary data
-    const followers = []
+    useEffect(() => {
+        async function getFollowers(userId) {
+            const response = await fetch(`http://localhost:5000/api/users/${userId}/followers`);
+            const data = await response.json();
+            setFollowers(data);
+        }
+
+        getFollowers(uid);
+    }, [uid]);
 
     return (
-        <UserGrid users={followers}/>
+        <>
+            <h1 className="text-2xl">Followers</h1>
+            {followers.length === 0 ? <span>No results</span> : null}
+            <UserGrid users={followers}/>
+        </>
     )
 }
 
--- src/pages/Following.jsx
+++ src/pages/Following.jsx
@@ -1,14 +1,29 @@
 import { useParams } from "react-router-dom";
 import UserGrid from "../components/UserGrid";
+import { useEffect, useState } from "react";
 
 const Following = () => {
-    const {uid} = useParams();
-    
-    // Temporary data
-    const following = []
+    const { uid } = useParams();
+
+    const [following, setFollowing] = useState([]);
+
+
+    useEffect(() => {
+        async function getFollowing(userId) {
+            const response = await fetch(`http://localhost:5000/api/users/${userId}/following`);
+            const data = await response.json();
+            setFollowing(data);
+        }
+
+        getFollowing(uid);
+    }, [uid])
 
     return (
-        <UserGrid users={following}/>
+        <>
+            <h1 className="text-2xl">Following</h1>
+            {following.length === 0 ? <span>No results</span> : null}
+            <UserGrid users={following}/>
+        </>
     )
 }
 
