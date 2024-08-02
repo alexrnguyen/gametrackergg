95193f7d18f841efc8eadf011c048758618460ea
alexrnguyen <alexander.nguyen@shaw.ca>
2024-08-01 19:21:27 -0600

    refactor ReviewCard to limit by characters instead of lines

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -5,12 +5,12 @@ import { useEffect, useState } from "react";
 const ReviewCard = ({id, game, text, user}) => {
     const [showMore, setShowMore] = useState(false);
     const [rating, setRating] = useState(0);
+    const CHAR_LIMIT = 500;
 
     useEffect(() => {
         // TODO: This logic does not work for any length of text
-        const textElement = document.querySelector(`[data-review="${id}"]`);
         const numChars = text.length;
-        if (numChars >= 500 && !textElement.classList.contains("line-clamp-none")) {
+        if (numChars >= CHAR_LIMIT) {
             setShowMore(true);
         }
 
@@ -25,10 +25,8 @@ const ReviewCard = ({id, game, text, user}) => {
         getUserRating(user.id);
     }, [id, text.length, game.id, user.id]);
 
-    function handleShowMore() {
-        const textElement = document.querySelector(`[data-review="${id}"]`);
-        textElement.classList.add("line-clamp-none");
-        setShowMore(false);
+    function limitChars(str, limit) {
+        return str.length > limit ? `${str.slice(0, CHAR_LIMIT)}...` : str;
     }
 
     return (
@@ -50,8 +48,8 @@ const ReviewCard = ({id, game, text, user}) => {
                         <Rating value={rating} precision={0.5} readOnly />
                         <span>{game.release_dates[0].y}</span>
                     </div>
-                    <p data-review={id} className="mt-4 max-w-full line-clamp-5">{text}</p>
-                    {showMore ? <span className="font-bold cursor-pointer" onClick={handleShowMore}>more</span> : null}
+                    <p data-review={id} className="mt-4 max-w-full">{showMore ? limitChars(text, 500) : text}</p>
+                    {showMore ? <span className="font-bold cursor-pointer" onClick={() => setShowMore(false)}>more</span> : null}
                 </div>
             {/* Likes will go here */}
             </div>
