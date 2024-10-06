5fbcae71df8efce3f4dce31357b7b051c179a3a8
alexrnguyen <alexander.nguyen@shaw.ca>
2024-10-06 16:53:16 -0600

    add navbar and footer to error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,9 +1,15 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 // Page displayed when an error occurs in the case where the error is not caught
 const ErrorPage = () => {
 
     return (
-        <span className="grid place-content-center text-2xl">Something went wrong. Please refresh the page and try again</span>
+        <div className='flex flex-col flex-nowrap min-h-screen'>
+            <Navbar/>
+            <span className="flex-grow grid place-content-center text-2xl">Something went wrong. Please refresh the page and try again</span>
+            <Footer/>
+        </div>
     )
 }
 
